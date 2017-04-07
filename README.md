0d3da7253378165b78253b4917c3034e75f20a0c
madmax <maxt369@yahoo.com>
2017-04-07 09:49:00 -0400

    Fixed build problems, have an XOR example working

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ Tests can be run with `mvn test`.
 
 Finally, the generated jar can be found in `target/<app-name-and-version>`.
 
+## Running
+
+I have encountered some issues with the classpath. This build does not produce a fat jar with all of the necessary dependencies included, simply because there are many and it takes too much time to build and too much space to store. To make sure the jvm can resolve the classes at runtime, you can run `mvn dependency:build-classpath` and place the output into some environmental variable, say `cp`. Then, launch Java with `java -cp "$cp:target/<app-name>" <other-args>` 
+
 ## Note
 Before committing, run `mvn clean` to remove build files from the working directory. 
 
--- src/main/java/mo/lma/App.java
+++ src/main/java/mo/lma/App.java
@@ -1,11 +1,80 @@
 package mo.lma;
 
-
+import org.apache.commons.math3.optimization.Weight;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.distribution.UniformDistribution;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+import java.io.IOException;
 
 public class App {
 
-    public static void main(String[] args) {
-        MultiLayerConfiguration configuration = new MultiLayerConfiguration();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        INDArray input = Nd4j.zeros(4, 2);
+        INDArray labels = Nd4j.zeros(4, 2);
+
+        // [0 0] -> [1 0]
+        input.putScalar(new int[] {0, 0}, 0);
+        input.putScalar(new int[] {0, 1}, 0);
+        labels.putScalar(new int[] {0, 0}, 1);
+        labels.putScalar(new int[] {0, 1}, 0);
+
+        // [1 0] -> [0 1]
+        input.putScalar(new int[] {1, 0}, 1);
+        input.putScalar(new int[] {1, 1}, 0);
+        labels.putScalar(new int[] {1, 0}, 0);
+        labels.putScalar(new int[] {1, 1}, 1);
+
+        // [0 1] -> [0 1]
+        input.putScalar(new int[] {2, 0}, 0);
+        input.putScalar(new int[] {2, 1}, 1);
+        labels.putScalar(new int[] {2, 0}, 0);
+        labels.putScalar(new int[] {2, 1}, 1);
+
+        // [1 1] -> [1 1]
+        input.putScalar(new int[] {3, 0}, 1);
+        input.putScalar(new int[] {3, 1}, 1);
+        labels.putScalar(new int[] {3, 0}, 1);
+        labels.putScalar(new int[] {3, 1}, 0);
+
+        DataSet ds = new DataSet(input, labels);
+
+        // define a network
+       MultiLayerConfiguration xorNetworkConf =
+               new NeuralNetConfiguration.Builder()
+               .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+               .updater(Updater.NESTEROVS).momentum(0.9)
+               .learningRate(0.1)
+               .iterations(1000)
+               .list(new DenseLayer.Builder()
+                               .nIn(2).nOut(2)
+                               .weightInit(WeightInit.DISTRIBUTION)
+                               .dist(new UniformDistribution(0, 1))
+                               .activation(Activation.SIGMOID).build(),
+                       new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                               .activation(Activation.SOFTMAX).nIn(2).nOut(2)
+                               .weightInit(WeightInit.DISTRIBUTION)
+                               .dist(new UniformDistribution(0, 1))
+                               .build())
+               .backprop(true).build();
+
+        MultiLayerNetwork net = new MultiLayerNetwork(xorNetworkConf);
+        net.init();
+
+        net.fit(ds);
+
+        INDArray output = net.output(ds.getFeatureMatrix());
+        System.out.println(output);
     }
 }
